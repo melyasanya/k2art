87c8b60f31642e4fdbf04c63f483898e89945048
melyasanya <werbolom@gmail.com>
2024-03-26 20:44:08 +0200

    about description

---
+++ src/components/AuthorDescr/AuthorDescr.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export const AuthorDescr = () => {
+  return (
+    <div className="w-[200px] md:w-[400px] xl:w-[600px]">
+      <h1>
+        Hi, I am Katya Korolyuk <br /> An artist
+      </h1>
+      <p className="text-[9px] md:text-[14px]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum
+        nostrum ea cumque perspiciatis soluta officia ducimus, inventore
+        necessitatibus laboriosam eius provident excepturi, consectetur vel a
+        blanditiis veritatis eligendi. Aperiam!
+      </p>
+      <div className="flex justify-start gap-[40px] mt-[20px]">
+        <button className="border-2 border-black">
+          <Link href={""}>To my works</Link>
+        </button>
+        <button className="border-2 border-black">
+          <Link href={""}>Contact me</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/sections/AboutArtist/AboutArtsit.tsx
+++ src/sections/AboutArtist/AboutArtsit.tsx
@@ -1,10 +1,12 @@
+import { AuthorDescr } from "@/components/AuthorDescr/AuthorDescr";
 import { AuthorImage } from "@/components/AuthorImage/AuthorImage";
 import { Container } from "@/components/Container/Container";
 
 export const AboutArtist = () => {
   return (
     <section>
-      <Container>
+      <Container className="flex justify-between shrink-0">
+        <AuthorDescr />
         <AuthorImage />
       </Container>
     </section>
